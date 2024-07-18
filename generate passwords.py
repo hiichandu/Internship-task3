a993a48d8843addd3f3dd74332aea0779ea53d0f
chandu <143522017+hiichandu@users.noreply.github.com>
2024-07-18 01:16:01 -0500

    Add files via upload

---
+++ generate passwords.py
@@ -0,0 +1,17 @@
+import secrets
+import string
+
+def generate_password(length=16):
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    while True:
+        password = ''.join(secrets.choice(alphabet) for i in range(length))
+        if (any(c.islower() for c in password)
+            and any(c.isupper() for c in password)
+            and any(c.isdigit() for c in password)
+            and any(c in string.punctuation for c in password)):
+            break
+    return password
+
+# Example usage:
+password = generate_password()
+print("Generated Password:", password)
